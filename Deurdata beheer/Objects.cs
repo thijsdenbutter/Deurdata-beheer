09a2902828553c9251c36dc8b5fdb200b3d02a0d
thijsdenbutter <thijsdenbutter@gmail.com>
2025-02-18 13:37:10 +0100

    object structuur gemaakt

--- Deurdata beheer/Objects.cs
+++ Deurdata beheer/Objects.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -15,6 +16,13 @@ namespace Deurdata_beheer
         public string Name { get; }
         public ExportNameAttribute(string name) => Name = name;
     }
+
+    public enum OpeningDirectionType
+    {
+        [Description("NormalLeftOpen")] NS,
+        [Description("NormalRightOpen")] ND,
+    }
+
 
     public class StructureParameters
     {
@@ -45,14 +53,20 @@ namespace Deurdata_beheer
 
     public class Rows
     {
+        [ExportName("RIFERIMENTO")]
+        public int Reference { get; set; } = 1;
+
+        [ExportName("DESCRIZIONE_RIGA")]
+        public string RowDescription { get; set; }
+
+        [ExportName("PROGETTO_DEFAULT")]
+        public string DefaultProject { get; set; } = "EMPTY";
+
         [ExportName("Posizione")]
         public int Position { get; set; }
 
         [ExportName("Quantità")]
         public int Quantity { get; set; }
-
-        [ExportName("Descrizione_Trattamento")]
-        public string TreatmentDescription { get; set; }
 
         [ExportName("Configurazione_Telaio")]
         public string FrameConfiguration { get; set; }
@@ -67,10 +81,10 @@ namespace Deurdata_beheer
         public string ShutterConfiguration { get; set; }
 
         [ExportName("Larghezza_Serramento")]
-        public decimal WindowWidth { get; set; }
+        public decimal Width { get; set; }
 
         [ExportName("Altezza_Serramento")]
-        public decimal WindowHeight { get; set; }
+        public decimal Height { get; set; }
 
         [ExportName("Forma_Esterna")]
         public string OuterShape { get; set; } = "RETTANGOLO";
@@ -100,7 +114,7 @@ namespace Deurdata_beheer
         public decimal BottomOverlap { get; set; } = 0;
 
         [ExportName("Apertura")]
-        public string OpeningDirection { get; set; }
+        public OpeningDirectionType OpeningDirection { get; set; }
 
         [ExportName("Note_Riga")]
         public string RowNotes { get; set; }
@@ -124,6 +138,36 @@ namespace Deurdata_beheer
         public int NumberOfRows { get; set; }
 
         public Rows Rows { get; set; }
+    }
+    public static class IniExporter
+    {
+        public static void WriteToIni<T>(T obj, string section, string filePath)
+        {
+            var lines = new List<string>();
+            var type = typeof(T);
+
+            foreach (var prop in type.GetProperties())
+            {
+                var attr = prop.GetCustomAttribute<ExportNameAttribute>();
+                if (attr != null)
+                {
+                    var key = attr.Name;
+                    var value = prop.GetValue(obj)?.ToString() ?? string.Empty;
+
+                    if (prop.PropertyType == typeof(OpeningDirectionType))
+                    {
+                        var enumValue = (OpeningDirectionType)prop.GetValue(obj);
+                        var enumMember = enumValue.GetType().GetField(enumValue.ToString());
+                        var enumAttr = enumMember.GetCustomAttribute<ExportNameAttribute>();
+                        value = enumAttr?.Name ?? value;
+                    }
+
+                    lines.Add($"ini.INIWrite{{{section}, {key}, {value}}}");
+                }
+            }
+
+            //File.AppendAllLines(filePath, lines);
+        }
     }
 
 }
